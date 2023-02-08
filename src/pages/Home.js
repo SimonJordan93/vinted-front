fcabb9ce59285c52d4d6ccddf9faf3a5b607806a
simonjordan93 <simon.m.jordan@gmail.com>
2023-02-08 18:15:05 +0100

    bloqué

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,45 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Home = () => {
-  return (
-    <div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo autem
-        repellat adipisci iure accusantium sint consequuntur ex voluptas
-        cupiditate saepe qui nostrum hic dolorem obcaecati nesciunt nemo,
-        distinctio facere quia!
-      </p>
-      <Link to="/offer/:id"> To Offer Page</Link>
-    </div>
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://lereacteur-vinted-api.herokuapp.com/offers"
+        );
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  });
+  return isLoading ? (
+    <p>Loading...</p>
+  ) : (
+    <section className="main container">
+      <h2>Articles Populaires</h2>
+      <div className="item-gallery">
+        {data.offers.map((offer, index) => {
+          return (
+            <div className="item-card" key={index}>
+              <div className="profile-info">
+                <div className="profile-pic">
+                  {/* <img src={offer.owner.account.avatar} alt="" /> */}
+                </div>
+                <div className="username">{offer.owner.account.username}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
